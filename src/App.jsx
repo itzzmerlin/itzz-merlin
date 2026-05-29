0fabae16364ca6ce56cc441285b106e5b2e0ecf4
itzz_merlin <winterzstudios@gmail.com>
2026-05-29 13:47:57 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -239,7 +239,7 @@ export default function Portfolio() {
           </a>
 
           <a
-            href="https://github.com/JBlizz"
+            href="https://github.com/itzzmerlin"
             target="_blank"
             rel="noreferrer"
             className={`transition-all p-2 rounded-full border flex items-center justify-center w-9 h-9
